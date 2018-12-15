3207224d0c29eb7c74c554f1c6fdb32c2c9fc401
Steve Labus <angrykiller@icloud.com>
2018-12-15 12:59:39 +0100

    Added database structure

---
+++ wildcircus.sql
@@ -0,0 +1,44 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Hôte: localhost (MySQL 5.5.5-10.3.8-MariaDB)
+# Base de données: wildcircus
+# Temps de génération: 2018-12-15 11:59:23 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Affichage de la table mails
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mails`;
+
+CREATE TABLE `mails` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `sender` varchar(255) NOT NULL DEFAULT '' COMMENT 'Nom de l''auteur',
+  `email` varchar(255) NOT NULL DEFAULT '' COMMENT 'Adresse mail de l''auteur',
+  `content` text NOT NULL COMMENT 'Contenu du mail',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
